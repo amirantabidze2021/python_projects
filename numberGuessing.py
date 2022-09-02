576ed68495f18ef52aba83f09204f4fb30a84183
amirantabidze2021 <81627817+amirantabidze2021@users.noreply.github.com>
2022-09-02 10:53:03 +0400

    Add files via upload

---
+++ numberGuessing.py
@@ -0,0 +1,36 @@
+import random
+
+top_of_range=input("Type a number : ")
+
+if top_of_range.isdigit():
+    top_of_range=int(top_of_range)
+
+    if top_of_range <=0:
+        print("Please tupe a number larger tahn 0 next time ")
+        quit()
+else:
+    print("Type a number  next time")
+    quit()
+
+
+random_number=random.randint(0,top_of_range)
+
+while True:
+    user_guess=input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess=int(user_guess)
+
+    else:
+        print("Type a number  next time")
+        continue
+
+    if user_guess==random_number:
+        print("You got it !")
+        break
+    else:
+        if user_guess > nandom_number:
+            print("You were above the number ! ")
+        else:
+            print("You were below the number")
+
+
