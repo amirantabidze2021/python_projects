d0b93d50ca2d9c191b69d20ba52d372cff869f7e
Amiran Tabidze <81627817+amirantabidze2021@users.noreply.github.com>
2024-02-18 10:59:47 +0400

    Fix

---
+++ prime_numbers.py
@@ -0,0 +1,15 @@
+"""
+martivi ricxvis povna
+by Amiran Tabidze
+"""
+
+def is_prime(n):
+    
+    if n <= 1:
+        return False  
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False 
+    return True  
+n=int(input("Enter a integer number:  "))
+print(is_prime(n))
